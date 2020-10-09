f64c71ee9b17f53cac49fb48bb8a6b27c7b224c6
aquova <abrick912@gmail.com>
2020-10-09 13:04:23 -0500

    Use custom docker image as base

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
+docker pull aquova/discord.py:1.3.4
 DIR=`dirname "$(readlink -f "$0")"`
-docker build -t aquova/autumn $DIR
-# Load this directory in container filespace as /autumn and run main
-docker run -v $DIR:/autumn -it aquova/autumn python3 /autumn/src/main.py
+docker run -v $DIR:/autumn -it aquova/discord.py python3 /autumn/src/main.py
